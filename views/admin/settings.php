c7cc58e98796f499dc0d1931790eb20492439c26
Shadowed Mists <ifendres@gmail.com>
2014-07-02 11:55:00 -0400

    moved position of display name

--- views/admin/settings.php
+++ views/admin/settings.php
@@ -3,9 +3,9 @@
 <form method="post">
     <label for="blog_name">Blog Name</label>
     <input type="text" name="blog_name" required maxlength="63" value="<?php echo $model['blog_name']; ?>" />
-    <label for="email">Email</label>
-    <input type="email" name="email" required maxlength="250" value="<?php echo $model['email']; ?>" />
     <label for="display_name">Your Name</label>
     <input type="text" name="display_name" required maxlength="63" value="<?php echo $model['display_name']; ?>" />
+    <label for="email">Email</label>
+    <input type="email" name="email" required maxlength="250" value="<?php echo $model['email']; ?>" />
     <input type="submit" name="submit" value="Save Blog Information" />
 </form>
