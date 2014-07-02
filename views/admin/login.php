188c8b6beb5b45e259d9a271fdd72b961729caae
Shadowed Mists <ifendres@gmail.com>
2014-07-02 15:07:40 -0400

    removed reset password on return from post

--- views/admin/login.php
+++ views/admin/login.php
@@ -4,6 +4,6 @@
     <label for="email">Email</label>
     <input type="email" name="email" required value="<?php echo $model['email']; ?>" />
     <label for="password">Password</label>
-    <input type="password" name="password" required value="<?php echo $model['password']; ?>" />
+    <input type="password" name="password" required />
     <input type="submit" name="submit" value="Login" />
 </form>
