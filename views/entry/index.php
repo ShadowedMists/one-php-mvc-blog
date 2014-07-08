acdf82ee15388b53d2b694988c5fa0491f06ffa9
Shadowed Mists <ifendres@gmail.com>
2014-07-08 11:25:37 -0400

    Updated styles, Parsedown loads only on Blog Entry

--- views/entry/index.php
+++ views/entry/index.php
@@ -1,12 +1,13 @@
 <?php
+    include "includes/Parsedown.php";
     $settings = $this->get_settings();
     $parsedown = new Parsedown();
     $entry = $model['entry'];
     $tags = $model['tags'];
 ?>
 
-<h1><?php echo $entry->title; ?></h1>
-<p><?php echo $this->get_age_string($entry->created), ' by ', $settings->display_name;?></p>
+<h1 class="title"><?php echo $entry->title; ?></h1>
+<p class="info"><?php echo $this->get_age_string($entry->created), ' by ', $settings->display_name;?></p>
 <div class="markdown">
     <?php 
         if(!empty($entry->image_url)) {
@@ -15,9 +16,11 @@
         echo $parsedown->text($entry->body); 
     ?>
 </div>
+<?php if(count($tags) > 0 ) { ?>
 <div>
     <span style="font-weight:bold">Tags:</span>
     <?php foreach ($tags as $tag) { ?>
     <a href="<?php echo $this->route_url(NULL, 'tag', $tag); ?>"><?php echo $tag; ?></a>
     <?php } ?>
 </div>
+<?php } ?>
--- views/home/index.php
+++ views/home/index.php
@@ -22,8 +22,8 @@
 </div>
 <?php foreach($entries as $entry) { ?>
     <div class="entry">
-        <h2><a href="<?php echo $this->route_url(NULL, 'entry', $entry->id);?>"><?php echo $entry->title; ?></a></h2>
-        <p><?php echo $this->get_age_string($entry->created), ' by ', $settings->display_name; ?></p>
+        <h2 class="title"><a href="<?php echo $this->route_url(NULL, 'entry', $entry->id);?>"><?php echo $entry->title; ?></a></h2>
+        <p class="info"><?php echo $this->get_age_string($entry->created), ' by ', $settings->display_name; ?></p>
         <p class="snippet"><?php echo $entry->snippet; ?></>
     </div>
 <?php } ?>
